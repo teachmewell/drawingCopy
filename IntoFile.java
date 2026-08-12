70f9bc5b635e66bd2ee9617573c7c9f22d03841d
teachmewell <coyu3@duck.com>
2026-08-12 18:21:41 +0200

    Fix pixel dimensions and access in saveImage method
    Swapped height and width assignments in saveImage method and adjusted pixel access accordingly.

--- IntoFile.java
+++ IntoFile.java
@@ -7,14 +7,14 @@ public class IntoFile {
 
     public static void saveImage(int[][] pixels /*, String filename*/ ) {
 
-        int height = pixels.length;
-        int width = pixels[0].length;
+        int height = pixels[0].length;
+        int width = pixels.length;
 
         BufferedImage image= new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                image.setRGB(x, y, pixels[y][x]);
+                image.setRGB(x, y, pixels[x][y]);
             }
         }
 
@@ -34,11 +34,11 @@ public class IntoFile {
             int width = image.getWidth();
             int height = image.getHeight();
 
-            int[][] pixels = new int[height][width];
+            int[][] pixels = new int[width][height];
 
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
-                    pixels[y][x] = image.getRGB(x, y) & 0xFFFFFF;
+                    pixels[x][y] = image.getRGB(x, y) & 0xFFFFFF;
                 }
             }
             return pixels;
@@ -47,4 +47,7 @@ public class IntoFile {
             return null;
         }
     }
+
+
+   
 }
