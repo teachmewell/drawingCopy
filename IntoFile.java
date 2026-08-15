52405ee8e49f2b50f676a7a43feae4dcb53e1557
teachmewell <coyu3@duck.com>
2026-08-15 23:48:41 +0200

    Update IntoFile.java

--- IntoFile.java
+++ IntoFile.java
@@ -25,6 +25,26 @@ public class IntoFile {
         }
     }
 
+    public static void saveImage(int[][] pixels , String filename ) {
+
+        int height = pixels[0].length;
+        int width = pixels.length;
+
+        BufferedImage image= new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                image.setRGB(x, y, pixels[x][y]);
+            }
+        }
+
+        try {
+            ImageIO.write(image, "png", new File(filename));
+        } catch (IOException e) {
+              e.printStackTrace();
+        }
+    }
+
 
 
     public static int[][] loadImage(String filename) {
